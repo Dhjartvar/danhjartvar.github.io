2f9f15299c10a54ff0cea255c081ba2c9c3bd559
Dan <do@intelligentbanker.com>
2019-12-20 01:54:58 +0100

    Feat: Wrapper layout with header toolbar for desktop and footer toolbar for mobile

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="bg-secondary">
+    <q-header elevated id="content-desktop">
+      <Toolbar :isMobile="false" />
+    </q-header>
+
+    <q-footer id="content-mobile">
+      <Toolbar :isMobile="true" />
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+#content-desktop {
+  display: block;
+}
+#content-mobile {
+  display: none;
+}
+
+@media screen and (max-width: 768px) {
+  #content-desktop {
+    display: none;
+  }
+  #content-mobile {
+    display: block;
+  }
+}
+</style>
+
+<script>
+import Toolbar from '../components/layouts/Toolbar'
+
+export default {
+  name: 'Layout',
+  components: {
+    Toolbar
+  }
+}
+</script>
