48d8c584669e4b54f2d62bb7218a936b327d9807
Dan <do@intelligentbanker.com>
2020-01-02 14:13:06 +0100

    Feat: Education page

--- src/pages/Education.vue
+++ src/pages/Education.vue
@@ -1,9 +1,27 @@
 <template>
-  <q-page class="flex flex-center"></q-page>
+  <q-page :class="small ? 'smallPage' : 'page'">
+    <EducationTimeline />
+  </q-page>
 </template>
 
+<style>
+.card {
+  margin-bottom: 2%;
+}
+</style>
+
 <script>
+import EducationTimeline from '../components/education/EducationTimeline'
+
 export default {
-  name: 'EducationPage'
+  name: 'EducationPage',
+  components: {
+    EducationTimeline
+  },
+  computed: {
+    small() {
+      return this.$q.screen.lt.md
+    }
+  }
 }
 </script>
