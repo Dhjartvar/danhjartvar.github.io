800db269b7cb4682a85e3e489f99f41b62c66ae8
Dan <do@intelligentbanker.com>
2020-01-02 14:12:16 +0100

    Feat: Course component

---
+++ src/components/education/Course.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-item>
+    <q-item-section avatar style="height: 32px;">
+      <q-icon color="accent" name="mdi-pencil" />
+    </q-item-section>
+    <q-item-section>{{ label }}</q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'Course',
+  props: ['label']
+}
+</script>
